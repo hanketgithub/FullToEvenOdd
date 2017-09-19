9c439c3375b0357abda8a998774496682713280f
Hank Lee <hanlee.penn@gmail.com>
2017-09-19 15:58:20 +0800

    Add benchmark

--- main.c
+++ main.c
@@ -15,19 +15,15 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 #include "FullToEvenOdd.h"
 
+
 #define MAX_WIDTH   3840
 #define MAX_HEIGHT  2160
-
-
-typedef struct
-{
-    char name[256];
-} string_t;
 
 
 static uint8_t img[MAX_WIDTH * MAX_HEIGHT * 3 / 2];
@@ -38,11 +34,10 @@ static uint8_t buffer_bot_u[MAX_WIDTH * MAX_HEIGHT / 2 / 4];
 static uint8_t buffer_top_v[MAX_WIDTH * MAX_HEIGHT / 2 / 4];
 static uint8_t buffer_bot_v[MAX_WIDTH * MAX_HEIGHT / 2 / 4];
 
-static string_t null;
 
 int main(int argc, const char * argv[]) {
-    int fd_rd;
-    int fd_wr;
+    int ifd;
+    int ofd;
     
     ssize_t rd_sz;
     
@@ -53,8 +48,13 @@ int main(int argc, const char * argv[]) {
     uint8_t *src;
     
     char *cp;
-    string_t output;
-    
+    char output[256] = { 0 };
+
+    uint32_t frame_cnt;
+    struct timeval tv_start = { 0 };
+    struct timeval tv_end = { 0 };
+    struct timeval tv_interval = { 0 };
+
 
     if (argc < 4)
     {
@@ -67,18 +67,23 @@ int main(int argc, const char * argv[]) {
     width   = 0;
     height  = 0;
     cp      = NULL;
-    output  = null;
+    frame_cnt = 0;
     
     // get input file name from comand line
-    fd_rd = open(argv[1], O_RDONLY);
+    ifd = open(argv[1], O_RDONLY);
+    if (ifd < 0)
+    {
+        perror(argv[1]);
+        exit(EXIT_FAILURE);
+    }
     
     // specify output file name
     cp = strrchr(argv[1], '.');
-    strncpy(output.name, argv[1], cp - argv[1]);
-    strcat(output.name, "i");
-    strcat(output.name, cp);
+    strncpy(output, argv[1], cp - argv[1]);
+    strcat(output, "i");
+    strcat(output, cp);
     
-    fd_wr = open(output.name,
+    ofd = open(output,
                  O_WRONLY | O_CREAT | O_TRUNC,
                  S_IRUSR);
     
@@ -89,9 +94,10 @@ int main(int argc, const char * argv[]) {
     
     fprintf(stderr, "Processing: ");
 
+    gettimeofday(&tv_start, NULL);
     while (1)
     {
-        rd_sz = read(fd_rd, img, wxh * 3 / 2);
+        rd_sz = read(ifd, img, wxh * 3 / 2);
         
         if (rd_sz == wxh * 3 / 2)
         {
@@ -111,14 +117,16 @@ int main(int argc, const char * argv[]) {
             full_to_even_odd(width / 2, height / 2, buffer_top_v, buffer_bot_v, src);
             
             // Output Top
-            write(fd_wr, buffer_top_y, wxh / 2);
-            write(fd_wr, buffer_top_u, wxh / 2 / 4);
-            write(fd_wr, buffer_top_v, wxh / 2 / 4);
+            write(ofd, buffer_top_y, wxh / 2);
+            write(ofd, buffer_top_u, wxh / 2 / 4);
+            write(ofd, buffer_top_v, wxh / 2 / 4);
 
             // Output Bottom
-            write(fd_wr, buffer_bot_y, wxh / 2);
-            write(fd_wr, buffer_bot_u, wxh / 2 / 4);
-            write(fd_wr, buffer_bot_v, wxh / 2 / 4);
+            write(ofd, buffer_bot_y, wxh / 2);
+            write(ofd, buffer_bot_u, wxh / 2 / 4);
+            write(ofd, buffer_bot_v, wxh / 2 / 4);
+ 
+            frame_cnt++;
         }
         else
         {
@@ -127,12 +135,17 @@ int main(int argc, const char * argv[]) {
         fputc('.', stderr);
         fflush(stderr);
     }   
+    gettimeofday(&tv_end, NULL);
+    timersub(&tv_end, &tv_start, &tv_interval);
 
-    close(fd_rd);
-    close(fd_wr);
+    close(ifd);
+    close(ofd);
 
     fprintf(stderr, "Done\n");
-    fprintf(stderr, "Output file: %s\n", output.name);
+    fprintf(stderr, "Output file: %s\n", output);
+
+    double interval_in_double = ((double) tv_interval.tv_sec * 1000000 + tv_interval.tv_usec) / 1000000;
+    fprintf(stderr, "Performance: %f frames per second\n", frame_cnt / interval_in_double);
     
     return 0;
 }
